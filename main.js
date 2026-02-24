a410bddf9e686fd6e470ce5525564a7094d7ed97
hillo <hillo3127@gmail.com>
2026-02-24 16:44:27 +1300

    table times

--- main.js
+++ main.js
@@ -1,18 +1,69 @@
+//Init People
+document.addEventListener("DOMContentLoaded", function () {
 
-var Date = new Date().toISOString().split('T')[0];
-document.getElementById("main").innerHTML = Date;
+  fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
 
-/*
-get_data() {O
-  var people = [];
-   getJSON('people.json', function(data) {
-       $.each(data.person, function(i, f) {
-          var tblRow = "<tr>" + "<td>" + f.firstName + "</td>" +
-           "<td>" + f.lastName + "</td>" + "<td>" + f.job + "</td>" + "<td>" + f.roll + "</td>" + "</tr>"
-           $(tblRow).appendTo("#userdata tbody");
-     });
+      const tableBody = document.querySelector("#userTable tbody");
 
-   });
+      data.people.forEach(person => {
+
+
+        const row = document.createElement("tr");
+        
+        row.innerHTML = `
+          <td>${person.Name}</td>
+          <td id = ${person.Name}></td>
+        `;
+
+        tableBody.appendChild(row);
+        clock(person.Name, person.Rel-Time)
+      });
+
+    })
+    .catch(error => console.error("Error loading JSON:", error));
 
 });
-*/
+
+
+//Update people
+function updateTimes() {
+   fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+      data.people.forEach(person => {
+        clock(person.Name, person.Rel-Time)
+      });
+    })
+
+}
+
+
+//Clock format & math
+function clock(target, difference){
+  const now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes().toString();
+  let seconds = now.getSeconds().toString();
+
+  let state = ""
+  if(Math.floor((hours+difference)/2)%2 == 1){
+     state = "am"
+
+  }
+  else{
+    state = "pm"
+  }
+  
+  hours = ((hours + difference)%12).toString();
+
+  document.getElementById(target).innerHTML = `${hours}:${minutes}:${seconds} ${state}`;
+}
+
+
+clock("main", 0)
+
+setInterval(updateTimes, 10000);
+
